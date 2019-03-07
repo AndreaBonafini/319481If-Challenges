c207a70bdf2fe2fee11ecdfbbbae4389583a33c1
AndreaBonafini <47528585+AndreaBonafini@users.noreply.github.com>
2019-03-07 15:12:45 -0500

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -42,20 +42,203 @@ namespace _319481IfChallenges
                 {
                     Console.WriteLine("{0} is the Greatest number", b);
                 }
-                if (b > c)
-                {
-                    Console.WriteLine("{0} is the Gretest number", b);
-                }
-                else
-                {
-                    Console.WriteLine("{0} is the Gretest number", c);
-                }
-              
+
             }
             else
             {
-                Console.WriteLine("{0} is the Gretest number", c);
+                Console.WriteLine("{0} is the Greatest number", c);
             }
+            if (a > 0)
+            {
+                Console.WriteLine("{0} is positive", a);
+            }
+            else
+            {
+                if (a == 0)
+                {
+                    Console.WriteLine("{0} is zero", a);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is negative", a);
+                }
+            }
+
+            if (b > 0)
+            {
+                Console.WriteLine("{0} is positive", b);
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("{0} is zero", b);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is negative", b);
+                }
+            }
+            if (c > 0)
+            {
+                Console.WriteLine("{0} is positive", c);
+            }
+            else
+            {
+                if (c == 0)
+                {
+                    Console.WriteLine("{0} is zero", c);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is negative", c);
+                }
+            }
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("{0} is an even number", a);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd number", a);
+            }
+            if (b % 2 == 0)
+            {
+                Console.WriteLine("{0} is an even number", b);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd number", b);
+            }
+            if (c % 2 == 0)
+            {
+                Console.WriteLine("{0} is an even number", c);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd number", c);
+            }
+            
+            if (a % 5 ==0)
+            {
+                if (a % 11 == 0)
+                {
+                    Console.WriteLine("{0} is divisible by 5 and 11", a);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is divisible by 5", a);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} is not divisible by 5 and 11", a);
+            }
+            if (b % 5 == 0)
+            {
+                if (b % 11 == 0)
+                {
+                    Console.WriteLine("{0} is divisible by 5 and 11", b);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is divisible by 5", b);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} is not divisible by 5 and 11", b);
+            }
+            if (c % 5 == 0)
+            {
+                if (c % 11 == 0)
+                {
+                    Console.WriteLine("{0} is divisible by 5 and 11", c);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is divisible by 5", c);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} is not divisible by 5 and 11", c);
+            }
+
+
+
+        }
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Text = txtInput.Text.ToLower();
+            char theLetter = txtInput.Text[0];
+            bool isVowel = false;
+            if (theLetter == 'a')
+            {
+                isVowel = true;
+            }
+            else if (theLetter == 'e')
+            {
+                isVowel = true;
+            }
+            else if (theLetter == 'i')
+            {
+                isVowel = true;
+            }
+            else if (theLetter == 'o')
+            {
+                isVowel = true;
+            }
+            else if (theLetter == 'u')
+            {
+                isVowel = true;
+            }
+
+            if(isVowel)
+                {
+                    lblOutput.Content = " Vowel";
+                }
+            else
+                {
+                    lblOutput.Content = "Consonant";
+                }
+
+
+
+            }
+
+        private void btnLeapYear_Click(object sender, RoutedEventArgs e)
+        {
+            int a;
+            int.TryParse(txtYear.Text, out a);
+            if (a % 4 == 0)
+            {
+                lblOutput1.Content = " The year is a leap year";
+            }
+            else
+            {
+                lblOutput1.Content = "The year is not a leap year";
+            }
+        }
+
+        private void Character_Click(object sender, RoutedEventArgs e)
+        {
+            int a;
+            int.TryParse(txtYear.Text, out a);
+            if (a >= 0)
+            { if (a < 0)
+                {
+                    lblCharacter.Content = "a is a number";
+                }
+                else
+                {
+                    {
+                        lblCharacter.Content = "a is a letter";
+                    }
+                }
+           
         }
     }
 }
